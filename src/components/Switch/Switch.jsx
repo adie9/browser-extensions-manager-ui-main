6a7801d29b741d999db77a1d335ac344832fcf67
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-08-28 14:11:52 -0400

    Created Switch folder

---
+++ src/components/Switch/Switch.jsx
@@ -0,0 +1,13 @@
+export default function Switch() {
+    const [isChecked, setIsChecked] = useState(false);
+
+    function handleCheckboxChange() {
+        setIsChecked(!isChecked);
+    }
+    
+    return (
+        <>
+            
+        </>
+    )
+}
