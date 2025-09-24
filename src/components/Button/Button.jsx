1ceaeef70ef30bc98dd2e7008ca10762541d20e6
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-09-24 10:32:04 -0400

    Changed hover state for remove button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import './button.css';
-export default function Button({id, label, active, onClick}) {
+export default function Button({id, label, active, onClick, className = ""}) {
     return(
         <>
             <button
              onClick={() => onClick(id)}
-             className={`btn shadow ${active ? "btn-active" : ""}`}>
+             className={`btn shadow ${active ? "btn-active" : ""} ${className}`}>
                 {label}
             </button>
         </>
